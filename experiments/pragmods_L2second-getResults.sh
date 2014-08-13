feac04a5426188a340f8c31dbe3f8837b78c17f9
Avery Katko <averykatko@gmail.com>
2014-08-12 18:50:21 -0700

    L2sec settings

--- experiments/pragmods_L2second-getResults.sh
+++ experiments/pragmods_L2second-getResults.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-pushd /Profiles/akatko/Documents/aws-mturk-clt-1.3.1/bin
-./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second.success -outputfile /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second.results.tsv
+pushd /Applications/aws-mturk-clt-1.3.1/bin
+./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/Avery/Documents/pragmods/experiments/pragmods_L2second.success -outputfile /Users/Avery/Documents/pragmods/experiments/pragmods_L2second.results.tsv
 popd
--- experiments/pragmods_L2second-postHIT.sh
+++ experiments/pragmods_L2second-postHIT.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-pushd /Profiles/akatko/Documents/aws-mturk-clt-1.3.1/bin
-./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second -input /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second.input -question /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second.question -properties /Profiles/akatko/Documents/repos/pragmods/experiments/pragmods_L2second.properties -maxhits 1
+pushd /Applications/aws-mturk-clt-1.3.1/bin
+./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Users/Avery/Documents/pragmods/experiments/pragmods_L2second -input /Users/Avery/Documents/pragmods/experiments/pragmods_L2second.input -question /Users/Avery/Documents/pragmods/experiments/pragmods_L2second.question -properties /Users/Avery/Documents/pragmods/experiments/pragmods_L2second.properties -maxhits 1
 popd
